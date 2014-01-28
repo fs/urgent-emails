737e2203c2eddefd5f67ebf3677e2f38830f59b2
Vladimir Mikhailov <vladimir.mikhailov@flatstack.com>
2014-01-28 14:48:40 +0400

    We should be able to set day to perform rake task in arguments. Added for heroku scheduler

--- lib/tasks/notifications/deliver.rb
+++ lib/tasks/notifications/deliver.rb
@@ -1,7 +1,13 @@
 namespace :notifications do
   desc 'Deliver notifications'
-  task deliver: :environment do
-    DeliverEmails.new.perform mailer: NotificationsMailer,
-      retriever: RetrieveEmails.new
+  task :deliver, [:day_to_perform] => :environment do |t, args|
+    args.with_defaults(day_to_perform: :friday)
+
+    if Time.now.respond_to?("#{args[:day_to_perform]}?") &&
+      Time.now.public_send("#{args[:day_to_perform]}?")
+
+      DeliverEmails.new.perform mailer: NotificationsMailer,
+        retriever: RetrieveEmails.new
+    end
   end
 end
