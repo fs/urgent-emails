62e71f3d8f59b1e91dd7dda00fa606c7c9d0ebe6
Vladimir Mikhailov <vladimir.mikhailov@flatstack.com>
2014-02-17 18:46:42 +0400

    Use EMAILS env variable instead of file encryption

--- app/services/retrieve_emails.rb
+++ app/services/retrieve_emails.rb
@@ -1,5 +1,4 @@
 class RetrieveEmails
-  FILE = File.join(app_config.root, 'lib/emails.text')
   EMAIL_REG=/\A[^@]+@[^@]+\z/
 
   def emails
@@ -9,20 +8,12 @@ class RetrieveEmails
   private
 
   def read_emails(block)
-    decrypt_emails
-
-    File.open(FILE) do |file|
-      while email = file.gets
-        add_valid_email(email.chomp, block)
-      end
+    (ENV['EMAILS'] || '').split(',').each do |email|
+      add_valid_email(email.chomp, block)
     end
   end
 
   def add_valid_email(email, block)
     block << email if email =~ EMAIL_REG
-  end
-
-  def decrypt_emails
-    `bin/encrypt --decrypt -f #{FILE} -k #{ENV['APP_KEY_SECRET']}`
   end
 end
